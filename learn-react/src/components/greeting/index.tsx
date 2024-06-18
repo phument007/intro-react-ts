0d5a68e6ec6a534e7fdbeba470a7d04cebd5fe96
phument007 <phumentgaming@gmail.com>
2024-06-19 01:27:54 +0700

    crate new component

---
+++ learn-react/src/components/greeting/index.tsx
@@ -0,0 +1,9 @@
+function Greeting(): any {
+
+    return (
+        <div>
+            <h1>Hello React Type Script</h1>
+        </div>
+    );
+}
+export default Greeting;
--- learn-react/src/main.tsx
+++ learn-react/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Greeting from './components/greeting'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
+    <Greeting/>
   </React.StrictMode>,
 )
